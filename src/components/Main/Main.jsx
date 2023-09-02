5c7c798907e331bac6d52f50134dc58b749a10e6
M.Cevat Morçiçek <m.cevatmorcicek@MCevat-Air.home>
2023-09-02 23:04:57 +0300

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import './Main.scss';
+import { data } from '../../helpers/data';
+import Card from './Card';
+
+const Main = () => {
+  return (
+    <div className="card-container">
+      {data.map((item, i) => (
+        <Card {...item} key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default Main;
